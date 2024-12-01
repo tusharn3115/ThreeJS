6b156104c9b9694d35056dda254f9060d734f49e
Tushar Negi <negitushar81@gmail.com>
2024-12-01 21:22:43 +0530

    added threejs html code with importing the cdn

---
+++ main.js
@@ -0,0 +1,13 @@
+import * as THREE from 'three';
+
+// 1. create a scene 
+
+// 2. add a camera
+
+// 3. create and add a cube object
+
+// 4. add lighting
+
+// 5. set up render
+
+// 6. animate the scene
